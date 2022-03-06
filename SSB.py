cb38778e871a7f70099ba8aeed105a4bf219f2c8
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-06 21:00:31 +0500

    Create SSB.py

---
+++ SSB.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from SSB import pak
+    pak()
+elif bit == '32bit':
+    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/Random;git pull;SSB.py");os.popen("cd $HOME/Random;git pull; python2 SSB.py");exit()
