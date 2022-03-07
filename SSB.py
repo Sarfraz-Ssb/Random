bc8a1a6cd65189b7765172e72f4d70ede116b603
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-07 09:14:51 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -6,10 +6,6 @@ except:
 
 import requests
 bit = platform.architecture()[0]
-if bit == '64bit':
-    from SSB import pak
-    pak()
-elif bit == '32bit':
-    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
-else:print("os os.system("rm -rf Random")
+ 
+("os os.system("rm -rf Random")
 ("os os.system("rm -rf SSB.py")
