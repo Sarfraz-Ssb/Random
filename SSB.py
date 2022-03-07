1fe0d8cb05ddc71e5544e44e8be740d30f1d565c
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-07 09:07:23 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -12,3 +12,5 @@ if bit == '64bit':
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
 else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/Random;git pull;SSB.py");os.popen("cd $HOME/Random;git pull; python2 SSB.py");exit()
+
+  ("os system") rm -rf "(Random")
