7d0151dc65f0bf917ac0176bca14819b83557ee6
Sarfraz-Baloch <99527217+Sarfraz-Ssb@users.noreply.github.com>
2022-03-07 09:12:58 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -11,6 +11,5 @@ if bit == '64bit':
     pak()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
-else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/Random;git pull;SSB.py");os.popen("cd $HOME/Random;git pull; python2 SSB.py");exit()
-
-  ("os os.system("rm -rf Random")
+else:print("os os.system("rm -rf Random")
+("os os.system("rm -rf SSB.py")
